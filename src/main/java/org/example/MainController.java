b442d78cdd6c0816f54e3322a078a81ca9aa48dc
Eznamen <111395043+Eznamen@users.noreply.github.com>
2023-01-23 17:46:34 +0300

    added date

--- src/main/java/org/example/MainController.java
+++ src/main/java/org/example/MainController.java
@@ -5,12 +5,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.xml.crypto.Data;
+import java.text.DateFormat;
+import java.util.Date;
+
 @Controller
 public class MainController {
+    private DateFormat dateFormat;
 
     @GetMapping("/greeting") //http://localhost:8080/greeting
     public String greeting(@RequestParam(required = false, defaultValue = "World") String name, Model model) {
+        Date date = new Date();
+        dateFormat = DateFormat.getDateInstance(DateFormat.SHORT);
         model.addAttribute("name", name);
+        model.addAttribute("date", dateFormat.format(date));
         return "greeting";
     }
+//    public Date dateNow(Model model){
+//        Date date = new Date();
+//        model.addAttribute("date", date);
+//        return date;
+//    }
 }
